3342bacf69bbfc390efcb94bc2b6e391bd47b06a
Kseniia Moiseeva <mailtoksena@gmail.com>
2019-03-29 20:03:12 +0300

    **

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-  //swiper
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.popular__slider-btn--right',
-      prevEl: '.popular__slider-btn--left',
-    },
-  });
+ 
 });
